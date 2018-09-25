90056ed5c5dcee08c265c7f96f3dd23905172c6c
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-25 10:14:45 +0200

    Wersja nr2

--- src/Quiz.java
+++ src/Quiz.java
@@ -6,38 +6,44 @@ public class Quiz {
 
 
         Scanner quiz = new Scanner(System.in);
-        int guessNumber = guessNumber>100 && guessNumber<200&& guessNumber%3==0;
+        int guessNumber = 120;
         System.out.println("Podaj liczbę:");
         int userChoice = quiz.nextInt();
-        quiz.nextLine();
+
+
         while (userChoice < guessNumber) {
             System.out.println("Liczba jest za mała.");
-        }
+            System.out.println("Podaj liczbę:");
+            userChoice = quiz.nextInt();
+            quiz.nextLine();}
 
-        System.out.println("Podaj liczbę:");
-        userChoice = quiz.nextInt();
-        quiz.nextLine();
-        while (userChoice > guessNumber) {
-            System.out.println("Liczba jest za duża.");
-        }
 
-        System.out.println("Podaj liczbę:");
-        userChoice = quiz.nextInt();
-        quiz.nextLine();
-        while (userChoice % 3 != 0) {
-            System.out.println("Liczba nie jest podzielna przez 3.");
-        }
+            while (userChoice > guessNumber) {
+                System.out.println("Liczba jest za duża.");
+                System.out.println("Podaj liczbę:");
+                userChoice = quiz.nextInt();
+                quiz.nextLine();
+            }
 
-        System.out.println("Podaj liczbę:");
-        userChoice = quiz.nextInt();
-        quiz.nextLine();
-        while (userChoice == guessNumber) {
-            System.out.println("Twoja liczba jest poprawna!!!!Brawo!!!");
-            quiz.close();
-        }
+                while (userChoice % 3 != 0) {
+                    System.out.println("Liczba nie jest podzielna przez 3.");
+                    System.out.println("Podaj liczbę:");
+                    userChoice = quiz.nextInt();
+                    quiz.nextLine();
+                }
 
-    }
-}
+                    while (userChoice == guessNumber) {
+                        System.out.println("Twoja liczba jest poprawna!!!!Brawo!!!");
+                        userChoice = quiz.nextInt();
+                        quiz.nextLine();
+                        quiz.close();
+                    }
+
+                }
+            }
+
+
+
 
 
 
