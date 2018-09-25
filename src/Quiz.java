280d52532cebde46562b80b5c55816f3cb61e53e
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-25 10:44:02 +0200

    Do poprawy

--- src/Quiz.java
+++ src/Quiz.java
@@ -5,7 +5,6 @@ public class Quiz {
     public static void main(String[] args) {
 
 
-
         Scanner quiz = new Scanner(System.in);
         int guessNumber = 120;
         System.out.println("Podaj liczbę:");
@@ -16,31 +15,32 @@ public class Quiz {
             System.out.println("Liczba jest za mała.");
             System.out.println("Podaj liczbę:");
             userChoice = quiz.nextInt();
-            quiz.nextLine();}
+            quiz.nextLine();
+        }
 
 
-            while (userChoice > guessNumber) {
-                System.out.println("Liczba jest za duża.");
-                System.out.println("Podaj liczbę:");
-                userChoice = quiz.nextInt();
-                quiz.nextLine();
-            }
+        while (userChoice > guessNumber) {
+            System.out.println("Liczba jest za duża.");
+            System.out.println("Podaj liczbę:");
+            userChoice = quiz.nextInt();
+            quiz.nextLine();
+        }
 
-                while (userChoice % 3 != 0) {
-                    System.out.println("Liczba nie jest podzielna przez 3.");
-                    System.out.println("Podaj liczbę:");
-                    userChoice = quiz.nextInt();
-                    quiz.nextLine();
-                }
+        while (userChoice % 3 != 0) {
+            System.out.println("Liczba nie jest podzielna przez 3.");
+            System.out.println("Podaj liczbę:");
+            userChoice = quiz.nextInt();
+            quiz.nextLine();
+        }
 
-                    while (userChoice == guessNumber) {
-                        System.out.println("Twoja liczba jest poprawna!!!!Brawo!!!");
-                        userChoice = quiz.nextInt();
-                        quiz.close();
-                    }
+        while (userChoice == guessNumber) {
+            System.out.println("Twoja liczba jest poprawna!!!!Brawo!!!");
+            userChoice = quiz.nextInt();
+            quiz.nextLine();
+        }
+    }
+}
 
-                }
-            }
 
 
 
