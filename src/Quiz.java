59c982140a233b372f86adfdd5bf2b1f2f4d1f74
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-27 09:32:57 +0200

    Do poprawy1.2.

--- src/Quiz.java
+++ src/Quiz.java
@@ -6,51 +6,37 @@ public class Quiz {
 
 
         Scanner quiz = new Scanner(System.in);
-        int guessNumber = 120;
+
         System.out.println("Podaj liczbę:");
         int userChoice = quiz.nextInt();
+        int guessNumber = 25;
 
+        while (userChoice != guessNumber) {
 
-        while (userChoice < guessNumber) {
-            System.out.println("Liczba jest za mała.");
-            break;
+            if (userChoice < guessNumber) {
+                System.out.println("Liczba jest za mała");
+            }
+            if (userChoice > guessNumber){
+                System.out.println("Liczba jest za duża");
+            }
+            if (userChoice % 3 != 0){
+                System.out.println("Liczba nie jest podzielna przez 3");
+            }
+                userChoice = quiz.nextInt();
         }
-
-            System.out.println("Podaj liczbę:");
-            userChoice = quiz.nextInt();
-            quiz.nextLine();
-
-
-
-        while (userChoice > guessNumber) {
-            System.out.println("Liczba jest za duża.");
-            break;
-        }
-
-            System.out.println("Podaj liczbę:");
-            userChoice = quiz.nextInt();
-            quiz.nextLine();
-
-
-        while (userChoice % 3 != 0) {
-            System.out.println("Liczba nie jest podzielna przez 3.");
-            break;
-        }
-
-            System.out.println("Podaj liczbę:");
-            userChoice = quiz.nextInt();
-            quiz.nextLine();
-
-
         while (userChoice == guessNumber) {
-            System.out.println("Twoja liczba jest poprawna!!!!Brawo!!!");
+            System.out.println("Liczba jest ok");
             break;
         }
-            userChoice = quiz.nextInt();
-            quiz.nextLine();
+        System.out.println("Jesteś półbogiem!");
+        quiz.close();
 
     }
+
+
 }
+
+
 
 
 
